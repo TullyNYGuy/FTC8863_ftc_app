49254c7d0d1b5384180ce1673cd056e9739c6217
TullyNYGuy <glenn_ball@utilitek.com>
2016-11-08 00:55:54 -0500

    Add a shell (outline) for a Linear OpMode so it is easy to start a new one.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/GenericTest/LinearOpModeShell.java
@@ -0,0 +1,50 @@
+package org.firstinspires.ftc.teamcode.opmodes.GenericTest;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+/**
+ * This Opmode is a shell for a linear OpMode. Copy this file and fill in your code as indicated.
+ *
+ *
+ */
+@TeleOp(name = "Linear Op Mode Shell - do not run", group = "Test")
+//@Disabled
+public class LinearOpModeShell extends LinearOpMode {
+
+    // Put your variable declarations here
+
+    @Override
+    public void runOpMode() {
+
+
+        // Put your initializations here
+        
+        // Wait for the start button
+        telemetry.addData(">", "Press Start to run" );
+        telemetry.update();
+        waitForStart();
+
+        // Put your calls here - they will not run in a loop
+
+        while(opModeIsActive()) {
+
+            // Put your calls that need to run in a loop here
+
+            // Display the current value
+            //telemetry.addData("Motor Speed = ", "%5.2f", powerToRunAt);
+            //telemetry.addData("Encoder Count=", "%5d", motor.getCurrentPosition());
+            telemetry.addData(">", "Press Stop to end test." );
+
+            telemetry.update();
+            
+            idle();
+        }
+
+        // Put your cleanup code here - it runs as the application shuts down
+        telemetry.addData(">", "Done");
+        telemetry.update();
+
+    }
+}
