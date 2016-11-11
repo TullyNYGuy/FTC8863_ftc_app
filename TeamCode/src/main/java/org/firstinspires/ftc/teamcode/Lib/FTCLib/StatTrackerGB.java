8d5aea4238feb4f82812a623ed75207d74b94a26
TullyNYGuy <glenn_ball@utilitek.com>
2016-11-11 13:30:10 -0500

    Changed StatTrackerGB to add 2 more modes to process the values: do calculations on they fly in one method, and do calculation all at the end after they are known

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Lib/FTCLib/StatTrackerGB.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Lib/FTCLib/StatTrackerGB.java
@@ -1,5 +1,6 @@
 package org.firstinspires.ftc.teamcode.Lib.FTCLib;
 
+import java.util.ArrayList;
 
 public class StatTrackerGB {
 
@@ -22,7 +23,17 @@ public class StatTrackerGB {
     private double average;
     private int count;
 
+    // running sum
     private double sum;
+
+    // place to store all of the values. Arrays cannot be extended in Java. They are a fixed length.
+    // ArrayList can be extended.
+    // Note that an Arraylist is probably a lot slower than a true array.
+    private ArrayList<Double> list;
+
+    // flag to indicate if the values in the ArrayList have been processed to find max, min, average
+    // sum and count
+    boolean isProcessed = false;
 
     //*********************************************************************************************
     //          GETTER and SETTER Methods
@@ -33,22 +44,52 @@ public class StatTrackerGB {
 
     // These are all readonly properties so we only want a getter method
     public double getMaximum() {
+        if (!list.isEmpty()) {
+            // since the list was not empty I'm assuming the user chose not to use updateStats() or
+            // updateStatsUnified(). So I have to process all of the values that were stored in the
+            // Arraylist.
+            processArrayList();
+        }
         return maximum;
     }
 
     public double getMinimum() {
+        if (!list.isEmpty()) {
+            // since the list was not empty I'm assuming the user chose not to use updateStats() or
+            // updateStatsUnified(). So I have to process all of the values that were stored in the
+            // Arraylist.
+            processArrayList();
+        }
         return minimum;
     }
 
     public double getAverage() {
+        if (!list.isEmpty()) {
+            // since the list was not empty I'm assuming the user chose not to use updateStats() or
+            // updateStatsUnified(). So I have to process all of the values that were stored in the
+            // Arraylist.
+            processArrayList();
+        }
         return average;
     }
 
     public double getCount() {
+        if (!list.isEmpty()) {
+            // since the list was not empty I'm assuming the user chose not to use updateStats() or
+            // updateStatsUnified(). So I have to process all of the values that were stored in the
+            // Arraylist.
+            processArrayList();
+        }
         return count;
     }
 
     public double getSum() {
+        if (!list.isEmpty()) {
+            // since the list was not empty I'm assuming the user chose not to use updateStats() or
+            // updateStatsUnified(). So I have to process all of the values that were stored in the
+            // Arraylist.
+            processArrayList();
+        }
         return sum;
     }
 
@@ -60,11 +101,9 @@ public class StatTrackerGB {
     //*********************************************************************************************
     public StatTrackerGB() {
         // initialize the properties
-        maximum = 0;
-        minimum = 0;
-        average = 0;
-        count = 0;
-        sum = 0;
+        reset();
+        // create an arraylist that is empty
+        list = new ArrayList<Double>();
     }
 
     //*********************************************************************************************
@@ -107,6 +146,22 @@ public class StatTrackerGB {
         average = sum / count;
     }
 
+    /**
+     * Iterate across the stored values and calculate min, max and average
+     */
+    private void processArrayList() {
+        // only process the Arraylist once
+        if (!isProcessed) {
+            for (Double number : list) {
+                updateMax(number);
+                updateMin(number);
+                updateAverage(number);
+            }
+        }
+        // indicate the ArrayList has already been processed
+        isProcessed = true;
+    }
+
     //*********************************************************************************************
     //          MAJOR METHODS
     //
@@ -135,6 +190,55 @@ public class StatTrackerGB {
             updateMax(newValue);
             updateAverage(newValue);
         }
+    }
+
+    /**
+     * This method does the same thing as updateStats but does not call other methods to do it. It
+     * will be interesting to see how much faster this method runs.
+     * @param newValue
+     */
+    public void updateStatsUnified(double newValue) {
+        // The first time this is called there are no previous values so just set all of the
+        // stats to the new value.
+        if (count == 0) {
+            maximum = newValue;
+            minimum = newValue;
+            sum = newValue;
+            average = newValue;
+            count = 1;
+            // This is not the first time called so update the stats the normal way.
+        } else {
+            // update the max
+            if (newValue > maximum) {
+                maximum = newValue;
+            }
+
+            // update the min
+            if (newValue < minimum) {
+                minimum = newValue;
+            }
+
+            // update the average and the count
+            sum = sum + newValue;
+            count++;
+            average = sum / count;
+        }
+    }
+
+    public void updateList(double newValue) {
+        list.add(newValue);
+    }
+
+    /**
+     * Reset everything so that a fresh set of numbers can be tracked.
+     */
+    public void reset() {
+        maximum = 0;
+        minimum = 0;
+        average = 0;
+        sum = 0;
+        count = 0;
+        isProcessed = false;
     }
 
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/GenericTest/TestStatTrackerGB.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/GenericTest/TestStatTrackerGB.java
@@ -50,6 +50,11 @@ public class TestStatTrackerGB extends LinearOpMode {
         programTimer.reset();
         loopTimer.reset();
 
+        //****************************************************************
+        // first test processing the stream of values as they are generated
+        // do the calculations for each value in a set of small methods
+        //*****************************************************************
+
         // the loop quits when the user presses the stop button on the phone or when the time limit
         // has expired
         while(opModeIsActive() && programTimer.milliseconds() < programTimeLimit) {
@@ -58,6 +63,84 @@ public class TestStatTrackerGB extends LinearOpMode {
 
             // tracking the time between loop executions
             statTracker.updateStats(loopTimer.milliseconds());
+            // reset the timer to 0
+            loopTimer.reset();
+
+            // Display
+            telemetry.addData(">", "Press Stop to end test." );
+            telemetry.update();
+
+            // Let the background tasks run
+            idle();
+        }
+
+        // now that the loop has finished and data collection is done, display the results on the
+        // driver station phone
+        telemetry.addData("Minimum loop time = ", "%5.2f", statTracker.getMinimum());
+        telemetry.addData("Maximum loop time = ", "%5.2f", statTracker.getMaximum());
+        telemetry.addData("Average loop time = ", "%5.2f", statTracker.getAverage());
+        telemetry.addData("Total loop time = ", "%5.2f", statTracker.getSum());
+        telemetry.addData("Number of loops = ", "%5.2f", statTracker.getCount());
+        telemetry.update();
+
+        // give the user 5 seconds to look at the results displayed on the phone
+        sleep(5000);
+
+        //****************************************************************
+        // next test processing the value on the fly as each value is generated
+        // do the calculations in one big method to avoid the overhead of calling
+        // small methods
+        //*****************************************************************
+
+        programTimer.reset();
+
+        // the loop quits when the user presses the stop button on the phone or when the time limit
+        // has expired
+        while(opModeIsActive() && programTimer.milliseconds() < programTimeLimit) {
+
+            // Put your calls that need to run in a loop here
+
+            // tracking the time between loop executions
+            statTracker.updateStatsUnified(loopTimer.milliseconds());
+            // reset the timer to 0
+            loopTimer.reset();
+
+            // Display
+            telemetry.addData(">", "Press Stop to end test." );
+            telemetry.update();
+
+            // Let the background tasks run
+            idle();
+        }
+
+        // now that the loop has finished and data collection is done, display the results on the
+        // driver station phone
+        telemetry.addData("Minimum loop time = ", "%5.2f", statTracker.getMinimum());
+        telemetry.addData("Maximum loop time = ", "%5.2f", statTracker.getMaximum());
+        telemetry.addData("Average loop time = ", "%5.2f", statTracker.getAverage());
+        telemetry.addData("Total loop time = ", "%5.2f", statTracker.getSum());
+        telemetry.addData("Number of loops = ", "%5.2f", statTracker.getCount());
+        telemetry.update();
+
+        // give the user 5 seconds to look at the results displayed on the phone
+        sleep(5000);
+
+        //****************************************************************
+        // last test processing the stream of values after they are all known
+        // just store each value and then process them all at once.
+        // Will this have less impact on the loop?
+        //*****************************************************************
+
+        programTimer.reset();
+
+        // the loop quits when the user presses the stop button on the phone or when the time limit
+        // has expired
+        while(opModeIsActive() && programTimer.milliseconds() < programTimeLimit) {
+
+            // Put your calls that need to run in a loop here
+
+            // tracking the time between loop executions
+            statTracker.updateList(loopTimer.milliseconds());
             // reset the timer to 0
             loopTimer.reset();
 
