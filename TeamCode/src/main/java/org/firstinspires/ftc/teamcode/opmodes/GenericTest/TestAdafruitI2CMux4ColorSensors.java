7236c7ed52e77aec20c5c6316c8b1c8b97ab1e1b
TullyNYGuy <glenn_ball@utilitek.com>
2017-01-18 23:57:55 -0500

    Wrote code to test 4 color sensors hanging off the I2C mux Cleaned up comments in TestAdaFruitI2CMux and split LEDs into 2 separate LEDs

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/GenericTest/TestAdafruitI2CMux4ColorSensors.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/GenericTest/TestAdafruitI2CMux4ColorSensors.java
@@ -1,28 +1,30 @@
 package org.firstinspires.ftc.teamcode.opmodes.GenericTest;
 
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.I2cAddr;
 
 import org.firstinspires.ftc.teamcode.Lib.FTCLib.AdafruitColorSensor;
 import org.firstinspires.ftc.teamcode.Lib.FTCLib.AdafruitI2CMux;
 
 /**
  * This opmode demonstates how to use the AdafruitI2CMux class to talk to an I2C mux.
- * It assumes that you have 2 adafruit color sensors. One connected to port 0 on the mux board and the
- * other is connected to port 1.
- * The gamepad will be used to switch between the 2 sensors.
- * The phone needs to be configured with an I2C Device on the port the mux is connected to. The name
- * of the mux is "mux".
- * And it also needs an Adafruit Color Sensor on one of the other ports. It does not matter which
- * one. The name of the color sensor is "colorSensor".
- * Lastly, the core device interface module needs to be named "coreDIM".
+ * It assumes that you have 4 adafruit color sensors connected on these ports:
+ * colorSensor1 - port 0
+ * colorSensor2 - port 1
+ * colorSensor3 - port 2
+ * colorSensor4 - port 4
+ * The gamepad will be used to switch between the 4 sensors.
+ * The phone needs to be configured with an I2C Device on the core device interface module port
+ * that the mux is connected to. The name of the mux is "mux".
+ * It also needs an Adafruit Color Sensor configured on one of the other I2C ports of the core
+ * device interface module. It does not matter which port. The name of the color sensor is
+ * "colorSensor".
+ * Lastly, the core device interface module needs to be named "coreDIM" in your phone configuration.
  *
  */
-@TeleOp(name = "Test Adafruit I2C Mux", group = "Test")
+@TeleOp(name = "Test Adafruit I2C Mux 4 Color Sensors", group = "Test")
 //@Disabled
-public class TestAdafruitI2CMux extends LinearOpMode {
+public class TestAdafruitI2CMux4ColorSensors extends LinearOpMode {
 
     // Put your variable declarations here
     AdafruitI2CMux mux;
@@ -35,6 +37,8 @@ public class TestAdafruitI2CMux extends LinearOpMode {
     // This class integrates both the color sensor and the led control using a discrete output port.
     AdafruitColorSensor colorSensor1;
     AdafruitColorSensor colorSensor2;
+    AdafruitColorSensor colorSensor3;
+    AdafruitColorSensor colorSensor4;
     // The next variable will point to one of the two colorSensors above; the one that is active
     AdafruitColorSensor activeColorSensor;
     // The name of the color sensor. This needs to be used when configuring the phone.
@@ -42,12 +46,13 @@ public class TestAdafruitI2CMux extends LinearOpMode {
 
     // The name of the core device interface module. This needs to be used when configuring the phone.
     String coreDIMName = "coreDIM";
-    String activePort;
 
-    // Connect the wires for the led from each color sensor board to the discrete output pin on
-    // channels 4 and 5 of the core DIM.
-    final int CHANNEL_FOR_LED1 = 5;
-    final int CHANNEL_FOR_LED2 = 4;
+    // Connect the wires for the led from the color sensor board to the discrete output pin on
+    // channel of the core DIM listed below.
+    final int CHANNEL_FOR_LED1 = 1;
+    final int CHANNEL_FOR_LED2 = 2;
+    final int CHANNEL_FOR_LED3 = 3;
+    final int CHANNEL_FOR_LED4 = 4;
 
     boolean xButtonIsReleased = false;
     boolean ledState = false;
@@ -60,25 +65,38 @@ public class TestAdafruitI2CMux extends LinearOpMode {
         // disconnect all 8 ports
         mux.disablePorts();
 
-        // connect only port 0. A color sensor (colorSensor1) has been wired to that port.
+        // connect only port 0. A color sensor (colorSensor1) has been wired to that port of the mux.
         mux.selectAndEnableAPort(AdafruitI2CMux.PortNumber.PORT0);
         // create colorSensor1 and initialize it
         colorSensor1 = new AdafruitColorSensor(colorSensorName, coreDIMName, hardwareMap, CHANNEL_FOR_LED1);
 
-        // Note that there are 2 color sensor objects. But the phone and hardware map only know about
+        // Note that there are 4 color sensor objects. But the phone and hardware map only know about
         // one. That is because you are switching between them using the mux. I'm not sure if you could
         // get away with only 1 color sensor object but since I need to initialize both color sensors
-        // it is easier to use 2 objects.
+        // it is easier to use 4 objects.
 
-        // connect only port 1. A color sensor (colorSensor2) has been wired to that port.
+        // connect only port 1. A color sensor (colorSensor2) has been wired to that port of the mux.
         mux.selectAndEnableAPort(AdafruitI2CMux.PortNumber.PORT1);
-
         // create colorSensor2 and initialize it
         colorSensor2 = new AdafruitColorSensor(colorSensorName, coreDIMName, hardwareMap, CHANNEL_FOR_LED2);
+
+        // connect only port 2. A color sensor (colorSensor3) has been wired to that port of the mux.
+        mux.selectAndEnableAPort(AdafruitI2CMux.PortNumber.PORT2);
+        // create colorSensor2 and initialize it
+        colorSensor3 = new AdafruitColorSensor(colorSensorName, coreDIMName, hardwareMap, CHANNEL_FOR_LED3);
+
+
+        // connect only port 3. A color sensor (colorSensor4) has been wired to that port of the mux.
+        mux.selectAndEnableAPort(AdafruitI2CMux.PortNumber.PORT3);
+        // create colorSensor2 and initialize it
+        colorSensor4 = new AdafruitColorSensor(colorSensorName, coreDIMName, hardwareMap, CHANNEL_FOR_LED4);
+
+
         // connect only port 0. A color sensor has been wired to that port.
         mux.selectAndEnableAPort(AdafruitI2CMux.PortNumber.PORT0);
 
         // start off with the active colorSensor = colorSensor1
+        // This is used to point to one of the four color sensors; the one that is active
         activeColorSensor = colorSensor1;
         
         // Wait for the start button
@@ -94,6 +112,7 @@ public class TestAdafruitI2CMux extends LinearOpMode {
             if (gamepad1.x) {
                 if (xButtonIsReleased) {
                     if (!ledState) {
+                        // note the activeColorSensor points to the colorSensor object that is active
                         activeColorSensor.turnLEDOn();
                         ledState = true;
                     } else {
@@ -113,6 +132,8 @@ public class TestAdafruitI2CMux extends LinearOpMode {
             }
 
             // Use gamepad A to select the color sensor on port 1
+            // I'm using the alternative set of methods to select and enable a port just to
+            // show how they are used.
             if (gamepad1.a) {
                 mux.disablePorts();
                 mux.selectPort(AdafruitI2CMux.PortNumber.PORT1);
@@ -120,8 +141,20 @@ public class TestAdafruitI2CMux extends LinearOpMode {
                 activeColorSensor = colorSensor2;
             }
 
-            // Use gamepad B to disable all ports
+            // Use gamepad X to select the color sensor on port 2
+            if (gamepad1.x) {
+                mux.selectAndEnableAPort(AdafruitI2CMux.PortNumber.PORT2);
+                activeColorSensor = colorSensor3;
+            }
+
+            // Use gamepad B to select the color sensor on port 3
             if (gamepad1.b) {
+                mux.selectAndEnableAPort(AdafruitI2CMux.PortNumber.PORT3);
+                activeColorSensor = colorSensor4;
+            }
+
+            // Use gamepad right bumper to disable all ports
+            if (gamepad1.right_bumper) {
                 mux.disablePorts();
             }
 
