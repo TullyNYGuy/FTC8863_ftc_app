f3d8e6814a87333d13e65246871320b5047f9794
TullyNYGuy <glenn_ball@utilitek.com>
2017-01-19 12:40:22 -0500

    Started to write the update state machine

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Lib/VelocityVortex/FrontBeaconPusher.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Lib/VelocityVortex/FrontBeaconPusher.java
@@ -44,6 +44,8 @@ public class FrontBeaconPusher {
     private double frontRightServoCenterValueReverse = .5;
     private double deadband = .1;
 
+    private boolean complete = false;
+
     private AdafruitColorSensor8863 rightColorSensor;
 
     private double pusherMidPoint = 6.4/2; //measured in cm using the robot
@@ -92,34 +94,71 @@ public class FrontBeaconPusher {
     public void moveLeftPusherBackRightPusherBack() {
         leftCRServo.moveUntilLimitSwitch(CRServo.CRServoDirection.BACKWARD);
         rightCRServo.moveUntilLimitSwitch(CRServo.CRServoDirection.BACKWARD);
-        leftCRServo.update();
-        rightCRServo.update();
+        // set the next state
+        beaconPusherState = BeaconPusherState.BOTH_BACK;
+        // indicate that it has not gotten to the state yet
+        complete = false;
+        updateState();
     }
 
     public void moveLeftPusherForwardRightPusherForward() {
         leftCRServo.moveUntilLimitSwitch(CRServo.CRServoDirection.FORWARD);
         rightCRServo.moveUntilLimitSwitch(CRServo.CRServoDirection.FORWARD);
-        leftCRServo.update();
-        rightCRServo.update();
+        // set the next state
+        beaconPusherState = BeaconPusherState.BOTH_FORWARD;
+        // indicate that it has not gotten to the state yet
+        complete = false;
+        updateState();
     }
 
     public void moveLeftPusherForwardRightPusherBack() {
         leftCRServo.moveUntilLimitSwitch(CRServo.CRServoDirection.FORWARD);
         rightCRServo.moveUntilLimitSwitch(CRServo.CRServoDirection.BACKWARD);
-        leftCRServo.update();
-        rightCRServo.update();
+        // set the next state
+        beaconPusherState = BeaconPusherState.LEFT_FORWARD_RIGHT_BACK;
+        // indicate that it has not gotten to the state yet
+        complete = false;
+        updateState();
     }
 
     public void moveLeftPusherBackRightPusherForward() {
         leftCRServo.moveUntilLimitSwitch(CRServo.CRServoDirection.BACKWARD);
         rightCRServo.moveUntilLimitSwitch(CRServo.CRServoDirection.FORWARD);
-        leftCRServo.update();
-        rightCRServo.update();
+        // set the next state
+        beaconPusherState = BeaconPusherState.LEFT_BACK_RIGHT_FORWARD;
+        // indicate that it has not gotten to the state yet
+        complete = false;
+        updateState();
     }
 
     public void moveLeftPusherMidwayRightPusherMidway() {
 
     }
 
+    private BeaconPusherState updateState() {
+        leftCRServo.update();
+        rightCRServo.update();
+        switch(beaconPusherState) {
+            case BOTH_BACK:
+                // check to see if the movement to this state is complete
+                // if it is set the complete flag
+                // check to see if there has been a command to move to the middle
+                // if there is start the movement and set the next state
+                // check to see if the movement timer has expired
+                // if it has do what?
+                break;
+            case BOTH_MIDDLE:
+                break;
+            case LEFT_BACK_RIGHT_FORWARD:
+                break;
+            case LEFT_FORWARD_RIGHT_BACK:
+                break;
+            case BOTH_FORWARD:
+                break;
+            default:
+                break;
+        }
+        return beaconPusherState;
+    }
 
 }
