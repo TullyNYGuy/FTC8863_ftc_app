df0840e3e06b961ce3b1ee0f5d4cb45229092974
TullyNYGuy <glenn_ball@utilitek.com>
2017-12-29 11:24:59 -0500

    Renamed files. Completed and tested code.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Lib/FTCLib/DataLogging.java
@@ -0,0 +1,200 @@
+package org.firstinspires.ftc.teamcode.Lib.FTCLib;
+
+
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintStream;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import static java.lang.String.format;
+
+public class DataLogging {
+
+    //*********************************************************************************************
+    //          ENUMERATED TYPES
+    //
+    // user defined types
+    //
+    //*********************************************************************************************
+
+
+    //*********************************************************************************************
+    //          PRIVATE DATA FIELDS
+    //
+    // can be accessed only by this class, or by using the public
+    // getter and setter methods
+    //*********************************************************************************************
+
+    private ElapsedTime timer;
+    private PrintStream dataLog = null;
+    
+    private String folderPath = null;
+    
+    private String filePrefix = null;
+
+    private Telemetry telemetry;
+
+    private SimpleDateFormat dateFormat;
+
+    private boolean status;
+
+    //*********************************************************************************************
+    //          GETTER and SETTER Methods
+    //
+    // allow access to private data fields for example setMotorPower,
+    // getPositionInTermsOfAttachment
+    //*********************************************************************************************
+
+    public boolean isStatusOK() {
+        return status;
+    }
+
+    //*********************************************************************************************
+    //          Constructors
+    //
+    // the function that builds the class when an object is created
+    // from it
+    //*********************************************************************************************
+
+    public DataLogging(String folderPath, String filePrefix, Telemetry telemetry) {
+        this.folderPath = folderPath;
+        this.filePrefix = filePrefix;
+        dataLoggingSetup();
+    }
+    
+    public DataLogging(String filePrefix, Telemetry telemetry) {
+        this.folderPath = "/sdcard/FTC8863/";
+        this.filePrefix = filePrefix;
+        dataLoggingSetup();
+    }
+
+    private void dataLoggingSetup() {
+        boolean result = true;
+        // make sure the folder exists, if not create it, and create the log file itself
+        this.status = openDataLog(folderPath, filePrefix);
+        // create a timer for use in time stamping data
+        timer = new ElapsedTime();
+    }
+
+    //*********************************************************************************************
+    //          Helper Methods
+    //
+    // methods that aid or support the major functions in the class
+    //*********************************************************************************************
+
+    /**
+     * This method opens a log file for writing all the data and debug messages to it. The log file is written to the specified
+     * folder. The file name will be formed by concatenating the specified file prefix and a date-time stamp.
+     *
+     * @param folderPath specifies the folder path.
+     * @param filePrefix specifies the file name prefix.
+     * @return true if log file is successfully opened, false if it failed.
+     */
+    private boolean openDataLog(final String folderPath, final String filePrefix) {
+        boolean result = true;
+
+        // create a file name for the data log using a date and time suffix that is added to the
+        // filePrefix the user gave us.
+        this.dateFormat = new SimpleDateFormat("yyyy-MM-dd@HH-mm-ss", Locale.US);
+        String logFilePath = folderPath + "/" + filePrefix + "_" + dateFormat.format(new Date()) + ".log";
+        // create the folder that will hold the data log file
+        if (!mkdir(folderPath)) {
+            // can't create the folder
+            result = false;
+        } else {
+            result = openDataLog(logFilePath);
+        }
+        return result;
+    }
+
+    /**
+     * This method opens a log file for writing all the messages to it.
+     *
+     * @param dataLogFilePath specifies the data log file name.
+     * @return true if log file is successfully opened, false if it failed.
+     */
+    private boolean openDataLog(final String dataLogFilePath) {
+        boolean result = true;
+
+        try {
+            dataLog = new PrintStream(new File(dataLogFilePath));
+        }
+        catch (FileNotFoundException exception) {
+            telemetry.addData("Could not create log file: ", dataLogFilePath);
+            dataLog = null;
+            result = false;
+        }
+        dataLog.println("Time is in milliseconds!");
+        dataLog.println("Date and Time of log file start = " + dateFormat.format(new Date()));
+        return result;
+    }
+
+    /**
+     * If the directory/folder does not exist, create it.
+     * @param folderPath
+     * @return
+     */
+    private boolean mkdir(String folderPath) {
+        boolean result = true;
+
+        // see if the folder already exists
+        File folder = new File(folderPath);
+        if(folder.exists()) {
+            // the folder exists already so nothing to do
+            result = true;
+        } else {
+            // the folder does not exist so create it
+            try {
+                folder.mkdir();
+            }
+            catch (Exception exception) {
+                telemetry.addData("Could not make log file directory: ", folderPath);
+                dataLog = null;
+                result = false;
+            }
+        }
+        return result;
+    }
+
+    //*********************************************************************************************
+    //          MAJOR METHODS
+    //
+    // public methods that give the class its functionality
+    //*********************************************************************************************
+
+    /**
+     * Start the timer used for time stamping the data. Do this at the beginning of whatever you are
+     * tyring to log data for. For example, do this at the very beginning of autonomous.
+     */
+    public void startTimer() {
+        timer.reset();
+    }
+
+    /**
+     * Write a piece of data or a debug message into the log file. It will get time stamped.
+     * @param dataToLog
+     */
+    public void logData(String dataToLog) {
+        double timeStamp = timer.milliseconds();
+        String stringToWrite = "Time = " + String.format( "%.2f", timeStamp) + " " + dataToLog;
+        dataLog.println(stringToWrite);
+    }
+
+    /**
+     * This method closes the data log file. You have to do this after you are finished with logging.
+     */
+    public void closeDataLog() {
+        logData("Log File Complete");
+        if (dataLog != null) {
+            dataLog.close();
+            dataLog = null;
+        }
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/GenericTest/TestDataLogging.java
@@ -0,0 +1,56 @@
+package org.firstinspires.ftc.teamcode.opmodes.GenericTest;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.Lib.FTCLib.DataLogging;
+
+/**
+ * This Opmode is a shell for a linear OpMode. Copy this file and fill in your code as indicated.
+ *
+ *
+ */
+@TeleOp(name = "Test Data Logging", group = "Test")
+//@Disabled
+public class TestDataLogging extends LinearOpMode {
+
+    // Put your variable declarations here
+    DataLogging testDataLogging;
+
+    @Override
+    public void runOpMode() {
+
+
+        // Put your initializations here
+        testDataLogging = new DataLogging("test", telemetry);
+        if(testDataLogging.isStatusOK()) {
+            // Wait for the start button
+            telemetry.addData(">", "Press Start to run" );
+            telemetry.update();
+            waitForStart();
+
+            // Put your calls here - they will not run in a loop
+            testDataLogging.startTimer();
+
+            while(opModeIsActive()) {
+
+                // Put your calls that need to run in a loop here
+
+                testDataLogging.logData("Test");
+
+                telemetry.addData(">", "Press Stop to end test." );
+                telemetry.update();
+
+                idle();
+            }
+            testDataLogging.closeDataLog();
+            // Put your cleanup code here - it runs as the application shuts down
+            telemetry.addData(">", "Done");
+            telemetry.update();
+        } else {
+            telemetry.addData("Problem with log file", "!");
+            telemetry.addData("DONE", "!");
+            telemetry.update();
+        }
+    }
+}
