59e8a7a52203897e4b2581b7f4ba243363bf872b
TullyNYGuy <glenn_ball@utilitek.com>
2018-01-01 22:41:57 -0500

    Added robot to autonomous testing. Moved autonomous testing to autonomous opmode.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/RelicRecovery/TestAutonomousMovements.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/RelicRecovery/TestAutonomousMovements.java
@@ -1,5 +1,6 @@
 package org.firstinspires.ftc.teamcode.opmodes.RelicRecovery;
 
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
@@ -7,19 +8,20 @@ import org.firstinspires.ftc.teamcode.Lib.FTCLib.AdafruitIMU8863;
 import org.firstinspires.ftc.teamcode.Lib.FTCLib.DcMotor8863;
 import org.firstinspires.ftc.teamcode.Lib.FTCLib.DriveTrain;
 import org.firstinspires.ftc.teamcode.Lib.RelicRecoveryLib.GlyphDumper;
+import org.firstinspires.ftc.teamcode.Lib.RelicRecoveryLib.RelicRecoveryRobotStJohnFisher;
 import org.firstinspires.ftc.teamcode.opmodes.GenericTest.TestDrivingDistanceUsingIMURunToPosition;
 
 /**
  * This Opmode is a shell for a linear OpMode. Copy this file and fill in your code as indicated.
  */
-@TeleOp(name = "Test Autonomous Movements", group = "Test")
+@Autonomous(name = "Test Autonomous Movements", group = "Test")
 //@Disabled
 public class TestAutonomousMovements extends LinearOpMode {
 
     // Put your variable declarations here
 
-    public GlyphDumper glyphDumper;
-    DriveTrain driveTrain;
+    public RelicRecoveryRobotStJohnFisher robot;
+
     double correction;
     DriveTrain.Status statusDrive;
     public double actualTurnAngle;
@@ -30,10 +32,10 @@ public class TestAutonomousMovements extends LinearOpMode {
 
 
         // Put your initializations here
-        driveTrain = DriveTrain.DriveTrainAutonomous(hardwareMap, telemetry);
-        driveTrain.setCmPerRotation(31.9); // cm
-
-        glyphDumper = new GlyphDumper(hardwareMap, telemetry);
+        // create the robot
+        telemetry.addData("Initializing ...", "Wait for it ...");
+        telemetry.update();
+        robot = robot.createRobotForAutonomous(hardwareMap, telemetry);
 
         // Wait for the start button
         telemetry.addData(">", "Press Start to run");
@@ -45,8 +47,9 @@ public class TestAutonomousMovements extends LinearOpMode {
         // Put your cleanup code here - it runs as the application shuts down
         telemetry.addData("actual turn angle was ", "%3.2f", actualTurnAngle);
         telemetry.addData(">", "Done");
-        telemetry.addData("final Angle = ", "%3.1f", driveTrain.imu.getHeading());
+        telemetry.addData("final Angle = ", "%3.1f", robot.driveTrain.imu.getHeading());
         telemetry.update();
+        robot.shutdown();
         sleep(3000);
     }
 
@@ -58,9 +61,9 @@ public class TestAutonomousMovements extends LinearOpMode {
         spinTurn(-9.0, 0.1, AdafruitIMU8863.AngleMode.ABSOLUTE);
         //drive straight
         driveStraight(-75, 0.1);
-        glyphDumper.dump();
+        robot.glyphDumper.dump();
         sleep(1000);
-        glyphDumper.goHome();
+        robot.glyphDumper.goHome();
         driveStraight(-4, 0.1);
         sleep(1500);
         driveStraight(5, 0.1);
@@ -72,9 +75,9 @@ public class TestAutonomousMovements extends LinearOpMode {
         spinTurn(-18.5, 0.1, AdafruitIMU8863.AngleMode.ABSOLUTE);
         //drive straight
         driveStraight(-75, 0.1);
-        glyphDumper.dump();
+        robot.glyphDumper.dump();
         sleep(1000);
-        glyphDumper.goHome();
+        robot.glyphDumper.goHome();
         driveStraight(-5, 0.1);
         sleep(1500);
         driveStraight(6, 0.1);
@@ -86,9 +89,9 @@ public class TestAutonomousMovements extends LinearOpMode {
         spinTurn(-26.5, 0.1, AdafruitIMU8863.AngleMode.ABSOLUTE);
         //drive straight
         driveStraight(-85, 0.1);
-        glyphDumper.dump();
+        robot.glyphDumper.dump();
         sleep(1000);
-        glyphDumper.goHome();
+        robot.glyphDumper.goHome();
         driveStraight(-5, 0.1);
         sleep(1500);
         driveStraight(6, 0.1);
@@ -98,12 +101,12 @@ public class TestAutonomousMovements extends LinearOpMode {
         //turn on block
         //spinTurn(-9.0, 0.1, AdafruitIMU8863.AngleMode.ABSOLUTE);
         spinTurn(4.3, 0.1, AdafruitIMU8863.AngleMode.ABSOLUTE);
-        actualTurnAngle = driveTrain.imu.getHeading();
+        actualTurnAngle = robot.driveTrain.imu.getHeading();
         //drive straight
         driveStraight(-65, 0.1);
-        glyphDumper.dump();
+        robot.glyphDumper.dump();
         sleep(1000);
-        glyphDumper.goHome();
+        robot.glyphDumper.goHome();
         driveStraight(-10, 0.1);
         sleep(1500);
         driveStraight(15, 0.1);
@@ -114,10 +117,10 @@ public class TestAutonomousMovements extends LinearOpMode {
         //spinTurn(-9.0, 0.1, AdafruitIMU8863.AngleMode.ABSOLUTE);
         driveStraight(-67, 0.1);
         spinTurn(10, 0.1, AdafruitIMU8863.AngleMode.ABSOLUTE);
-        actualTurnAngle = driveTrain.imu.getHeading();
-        glyphDumper.dump();
+        actualTurnAngle = robot.driveTrain.imu.getHeading();
+        robot.glyphDumper.dump();
         sleep(1000);
-        glyphDumper.goHome();
+        robot.glyphDumper.goHome();
         driveStraight(-15,0.1);
         driveStraight(10, 0.1);
         //drive straight
@@ -135,10 +138,10 @@ public class TestAutonomousMovements extends LinearOpMode {
         //spinTurn(-9.0, 0.1, AdafruitIMU8863.AngleMode.ABSOLUTE);
         driveStraight(-55, 0.1);
         spinTurn(25, 0.1, AdafruitIMU8863.AngleMode.ABSOLUTE);
-        actualTurnAngle = driveTrain.imu.getHeading();
-        glyphDumper.dump();
+        actualTurnAngle = robot.driveTrain.imu.getHeading();
+        robot.glyphDumper.dump();
         sleep(1000);
-        glyphDumper.goHome();
+        robot.glyphDumper.goHome();
         driveStraight(-30,0.1);
         driveStraight(10, 0.1);
     }
@@ -148,10 +151,10 @@ public class TestAutonomousMovements extends LinearOpMode {
         //spinTurn(-9.0, 0.1, AdafruitIMU8863.AngleMode.ABSOLUTE);
         driveStraight(-55, 0.1);
         spinTurn(43, 0.1, AdafruitIMU8863.AngleMode.ABSOLUTE);
-        actualTurnAngle = driveTrain.imu.getHeading();
-        glyphDumper.dump();
+        actualTurnAngle = robot.driveTrain.imu.getHeading();
+        robot.glyphDumper.dump();
         sleep(1000);
-        glyphDumper.goHome();
+        robot.glyphDumper.goHome();
         driveStraight(-37,0.1);
         driveStraight(10, 0.1);
     }
@@ -159,10 +162,10 @@ public class TestAutonomousMovements extends LinearOpMode {
 
 
     public void driveStraight(double distance, double power) {
-        driveTrain.setupDriveDistance(power, distance, DcMotor8863.FinishBehavior.FLOAT);
+        robot.driveTrain.setupDriveDistance(power, distance, DcMotor8863.FinishBehavior.FLOAT);
 
         while (opModeIsActive()) {
-            statusDrive = driveTrain.updateDriveDistance();
+            statusDrive = robot.driveTrain.updateDriveDistance();
             if (statusDrive == DriveTrain.Status.COMPLETE) {
                 break;
             }
@@ -177,28 +180,28 @@ public class TestAutonomousMovements extends LinearOpMode {
     }
 
     public void spinTurn(double angle, double power, AdafruitIMU8863.AngleMode angleMode) {
-        driveTrain.setupTurn(angle, power, angleMode);
+        robot.driveTrain.setupTurn(angle, power, angleMode);
 
-        while (opModeIsActive() && !driveTrain.updateTurn()) {
+        while (opModeIsActive() && !robot.driveTrain.updateTurn()) {
             telemetry.addData(">", "Press Stop to end test.");
-            telemetry.addData("Angle = ", "%3.1f", driveTrain.imu.getHeading());
+            telemetry.addData("Angle = ", "%3.1f", robot.driveTrain.imu.getHeading());
             telemetry.update();
             idle();
         }
-        driveTrain.stopTurn();
-        telemetry.addData("Turn Angle = ", "%3.1f", driveTrain.imu.getHeading());
+        robot.driveTrain.stopTurn();
+        telemetry.addData("Turn Angle = ", "%3.1f", robot.driveTrain.imu.getHeading());
         telemetry.update();
     }
 
     public void driveDistanceUsingIMU(double heading, double power, double distance) {
-        driveTrain.setupDriveDistanceUsingIMU(heading, power, distance, AdafruitIMU8863.AngleMode.RELATIVE, 0, power, 2000);
+        robot.driveTrain.setupDriveDistanceUsingIMU(heading, power, distance, AdafruitIMU8863.AngleMode.RELATIVE, 0, power, 2000);
 
         telemetry.addData(">", "Press Stop to end test.");
         telemetry.update();
         sleep(1000);
 
         while (opModeIsActive()) {
-            boolean isDestinationReached = driveTrain.updateDriveDistanceUsingIMU();
+            boolean isDestinationReached = robot.driveTrain.updateDriveDistanceUsingIMU();
             //boolean isDestinationReached = true;
             if (isDestinationReached) {
                 //driveTrain.stopDriveDistanceUsingIMU();
