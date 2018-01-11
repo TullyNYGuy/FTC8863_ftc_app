0e021a9d7038b26fd9936dbfa4c5a18678df47ae
TullyNYGuy <glenn_ball@utilitek.com>
2018-01-11 12:16:26 -0500

    removed direct vuforia calls. Added ReadPictograph class and calls.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/RelicRecovery/AutonomousRedNonMat.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/RelicRecovery/AutonomousRedNonMat.java
@@ -21,6 +21,7 @@ import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackable;
 import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackableDefaultListener;
 import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackables;
 import org.firstinspires.ftc.teamcode.Lib.FTCLib.AllianceColor;
+import org.firstinspires.ftc.teamcode.Lib.RelicRecoveryLib.ReadPictograph;
 
 /**
  * This Opmode is a shell for a linear OpMode. Copy this file and fill in your code as indicated.
@@ -34,71 +35,15 @@ public class AutonomousRedNonMat extends AutonomousMethods {
     // Put your variable declarations here
     StartPosition startPosition = StartPosition.RED_NO_MAT;
     AllianceColor.TeamColor teamColor = AllianceColor.TeamColor.RED;
-
-    //**********************************************************************************************
-    // Vuforia declarations
-    //**********************************************************************************************
-    public static final String TAG = "Vuforia VuMark Sample";
-    OpenGLMatrix lastLocation = null;
-    /**
-     * {@link #vuforia} is the variable we will use to store our instance of the Vuforia
-     * localization engine.
-     */
-    VuforiaLocalizer vuforia;
-    //**********************************************************************************************
+    ReadPictograph readPictograph;
+    RelicRecoveryVuMark vuMark;
 
     @Override
     public void runOpMode() {
         // Put your initializations here
         createRobot();
 
-        //**********************************************************************************************
-        // Vuforia initializations
-        //**********************************************************************************************
-
-        /*
-         * To start up Vuforia, tell it the view that we wish to use for camera monitor (on the RC phone);
-         * If no camera monitor is desired, use the parameterless constructor instead (commented out below).
-         */
-        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
-        VuforiaLocalizer.Parameters parameters = new VuforiaLocalizer.Parameters(cameraMonitorViewId);
-
-        // OR...  Do Not Activate the Camera Monitor View, to save power
-        // VuforiaLocalizer.Parameters parameters = new VuforiaLocalizer.Parameters();
-
-        /*
-         * IMPORTANT: You need to obtain your own license key to use Vuforia. The string below with which
-         * 'parameters.vuforiaLicenseKey' is initialized is for illustration only, and will not function.
-         * A Vuforia 'Development' license key, can be obtained free of charge from the Vuforia developer
-         * web site at https://developer.vuforia.com/license-manager.
-         *
-         * Vuforia license keys are always 380 characters long, and look as if they contain mostly
-         * random data. As an example, here is a example of a fragment of a valid key:
-         *      ... yIgIzTqZ4mWjk9wd3cZO9T1axEqzuhxoGlfOOI2dRzKS4T0hQ8kT ...
-         * Once you've obtained a license key, copy the string from the Vuforia web site
-         * and paste it in to your code onthe next line, between the double quotes.
-         */
-        parameters.vuforiaLicenseKey = "AdFJpV3/////AAAAGfTNpwzHLEWLhTazSUptJDdvoaO1q58UH3Ix0gMjIizeGqeRTy/mHyHpZI3hX3VrQk0S4VJKsiwBIUrTZy57oWoQQGsD/6JXnrC/R2zQ2ruhxmV9JYc6zr5Lhu+aUFdce/WJezBkcUv7fD2y6kmNHAWlYyMx3ZP8YX2bSfTWu4PjiO3N/CFelgIJSz5BCRtYeFb1gKkCYhsqKUNfkWXznEFvX8ppW72yjbfq62QwqGFeuql/3cPce8asiOVo9NLiG9mIuADM+FWLairEHQ4h2euGHa+JNrk36EO0zVAFk9G2RBQJRkwgA7jUOGpCEW0Qqt0XJoM+0T0sOVORrn3Lqp9M4ecaQYsQsR8RPoZRL0Ox";
-
-        /*
-         * We also indicate which camera on the RC that we wish to use.
-         * Here we chose the back (HiRes) camera (for greater range), but
-         * for a competition robot, the front camera might be more convenient.
-         */
-        parameters.cameraDirection = VuforiaLocalizer.CameraDirection.FRONT;
-        this.vuforia = ClassFactory.createVuforiaLocalizer(parameters);
-
-        /**
-         * Load the data set containing the VuMarks for Relic Recovery. There's only one trackable
-         * in this data set: all three of the VuMarks in the game were created from this one template,
-         * but differ in their instance id information.
-         * @see VuMarkInstanceId
-         */
-        VuforiaTrackables relicTrackables = this.vuforia.loadTrackablesFromAsset("RelicVuMark");
-        VuforiaTrackable relicTemplate = relicTrackables.get(0);
-        relicTemplate.setName("relicVuMarkTemplate"); // can help in debugging; otherwise not necessary
-        telemetry.addData("Vuforia initialized", "!");
-        //**********************************************************************************************
+        readPictograph = new ReadPictograph(hardwareMap, telemetry);
 
         // Wait for the start button
         telemetry.addData(">", "Press Start to run" );
@@ -106,64 +51,21 @@ public class AutonomousRedNonMat extends AutonomousMethods {
         waitForStart();
 
         // Put your calls here - they will not run in a loop
+        readPictograph.runAtStart();
 
+        vuMark = readPictograph.getvuMark();
+        doAutonomousMovements(startPosition, vuMark);
 
         while(opModeIsActive()) {
             //robot.jewelArm.knockOffBall(teamColor);
-            //getPictograph();
+
 //            telemetry.addData("About to start autonomous movements", "!");
 //            telemetry.update();
 //            sleep(2000);
-            getAutonomousMovements(startPosition, RelicRecoveryVuMark.CENTER);
+
 //            telemetry.addData("Finished autonomous movements", "!");
 //            telemetry.update();
 //            sleep(2000);
-
-//            //**********************************************************************************************
-//            //Vuforia opmode
-//            //**********************************************************************************************
-//
-//            /**
-//             * See if any of the instances of {@link relicTemplate} are currently visible.
-//             * {@link RelicRecoveryVuMark} is an enum which can have the following values:
-//             * UNKNOWN, LEFT, CENTER, and RIGHT. When a VuMark is visible, something other than
-//             * UNKNOWN will be returned by {@link RelicRecoveryVuMark#from(VuforiaTrackable)}.
-//             */
-//            RelicRecoveryVuMark vuMark = RelicRecoveryVuMark.from(relicTemplate);
-//            if (vuMark != RelicRecoveryVuMark.UNKNOWN) {
-//
-//                /* Found an instance of the template. In the actual game, you will probably
-//                 * loop until this condition occurs, then move on to act accordingly depending
-//                 * on which VuMark was visible. */
-//                telemetry.addData("VuMark", "%s visible", vuMark);
-//
-//                /* For fun, we also exhibit the navigational pose. In the Relic Recovery game,
-//                 * it is perhaps unlikely that you will actually need to act on this pose information, but
-//                 * we illustrate it nevertheless, for completeness. */
-//                OpenGLMatrix pose = ((VuforiaTrackableDefaultListener)relicTemplate.getListener()).getPose();
-//                telemetry.addData("Pose", format(pose));
-//
-//                /* We further illustrate how to decompose the pose into useful rotational and
-//                 * translational components */
-//                if (pose != null) {
-//                    VectorF trans = pose.getTranslation();
-//                    Orientation rot = Orientation.getOrientation(pose, AxesReference.EXTRINSIC, AxesOrder.XYZ, AngleUnit.DEGREES);
-//
-//                    // Extract the X, Y, and Z components of the offset of the target relative to the robot
-//                    double tX = trans.get(0);
-//                    double tY = trans.get(1);
-//                    double tZ = trans.get(2);
-//
-//                    // Extract the rotational components of the target relative to the robot
-//                    double rX = rot.firstAngle;
-//                    double rY = rot.secondAngle;
-//                    double rZ = rot.thirdAngle;
-//                }
-//            }
-//            else {
-//                telemetry.addData("VuMark", "not visible");
-//            }
-//            //**********************************************************************************************
 
             // Put your calls that need to run in a loop here
 
