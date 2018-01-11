14b00746c6dd54598503d3bca9205ec11fb3e945
TullyNYGuy <glenn_ball@utilitek.com>
2018-01-11 12:41:58 -0500

    Broke MoveBySteps out of Servo8863 so I could test it without having a robot. This is not real code. Real code is in Servo8863.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/GenericTest/TestMoveBySteps.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/GenericTest/TestMoveBySteps.java
@@ -3,25 +3,30 @@ package org.firstinspires.ftc.teamcode.opmodes.GenericTest;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.DcMotor;
+
+import org.firstinspires.ftc.teamcode.Lib.FTCLib.MoveBySteps;
 
 /**
  * This Opmode is a shell for a linear OpMode. Copy this file and fill in your code as indicated.
  *
  *
  */
-@TeleOp(name = "Linear Op Mode Shell - do not run", group = "Test")
+@TeleOp(name = "Test Move By Steps", group = "Test")
 @Disabled
-public class TemplateLinearOpMode extends LinearOpMode {
+public class TestMoveBySteps extends LinearOpMode {
 
     // Put your variable declarations here
+    MoveBySteps moveBySteps;
+    boolean isComplete = false;
 
     @Override
     public void runOpMode() {
 
 
         // Put your initializations here
-        
+        moveBySteps = new MoveBySteps(telemetry);
+        moveBySteps.setupMoveBySteps(.92, .01, 100);
+
         // Wait for the start button
         telemetry.addData(">", "Press Start to run" );
         telemetry.update();
@@ -32,12 +37,9 @@ public class TemplateLinearOpMode extends LinearOpMode {
         while(opModeIsActive()) {
 
             // Put your calls that need to run in a loop here
+            moveBySteps.updateMoveBySteps();
 
-            // Display the current value
-            //telemetry.addData("Motor Speed = ", "%5.2f", powerToRunAt);
-            //telemetry.addData("Encoder Count=", "%5d", motor.getCurrentPosition());
             telemetry.addData(">", "Press Stop to end test." );
-
             telemetry.update();
             
             idle();
