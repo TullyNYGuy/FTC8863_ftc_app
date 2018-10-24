9afc204d07f721567a60bf9197757ea2c10db7b8
TullyNYGuy <glenn_ball@utilitek.com>
2018-10-24 00:04:26 -0400

    code that demonstrates how to be efficient while reading the color sensor

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/GenericTest/TestSensorREVColorDistanceTimes.java
@@ -0,0 +1,217 @@
+/* Copyright (c) 2017 FIRST. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted (subject to the limitations in the disclaimer below) provided that
+ * the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this list
+ * of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice, this
+ * list of conditions and the following disclaimer in the documentation and/or
+ * other materials provided with the distribution.
+ *
+ * Neither the name of FIRST nor the names of its contributors may be used to endorse or
+ * promote products derived from this software without specific prior written permission.
+ *
+ * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
+ * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.firstinspires.ftc.teamcode.opmodes.GenericTest;
+
+import android.app.Activity;
+import android.graphics.Color;
+import android.view.View;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.ColorSensor;
+import com.qualcomm.robotcore.hardware.DistanceSensor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+import org.firstinspires.ftc.teamcode.Lib.FTCLib.DataLogging;
+
+import java.util.Locale;
+
+/*
+This opmode shows the inefficiency of reading the color sensor multiple times. Reading the color
+sensor takes a lot of time - about 8 mSec per value read. So it is best to minimize the number of
+reads by using variables to store the values that get read. The first loop is not efficient because
+it does not use variables. The second loop is better because it uses variables to minimize the number
+of reads.
+ */
+@TeleOp(name = "Sensor: TestREVColorDistanceTimes", group = "Sensor")
+//@Disabled                            // Comment this out to add to the opmode list
+public class TestSensorREVColorDistanceTimes extends LinearOpMode {
+
+    /**
+     * Note that the REV Robotics Color-Distance incorporates two sensors into one device.
+     * It has a light/distance (range) sensor.  It also has an RGB color sensor.
+     * The light/distance sensor saturates at around 2" (5cm).  This means that targets that are 2"
+     * or closer will display the same value for distance/light detected.
+     *
+     * Although you configure a single REV Robotics Color-Distance sensor in your configuration file,
+     * you can treat the sensor as two separate sensors that share the same name in your op mode.
+     *
+     * In this example, we represent the detected color by a hue, saturation, and value color
+     * model (see https://en.wikipedia.org/wiki/HSL_and_HSV).  We change the background
+     * color of the screen to match the detected color.
+     *
+     * In this example, we  also use the distance sensor to display the distance
+     * to the target object.  Note that the distance sensor saturates at around 2" (5 cm).
+     *
+     */
+    ColorSensor sensorColor;
+    DistanceSensor sensorDistance;
+    DataLogging dataLogging;
+    int loopcount;
+
+    @Override
+    public void runOpMode() {
+
+        // get a reference to the color sensor.
+        sensorColor = hardwareMap.get(ColorSensor.class, "revColorSensor");
+        double red = 0;
+        double blue = 0;
+        double green = 0;
+        double argb = 0;
+        double distance =0;
+
+        // get a reference to the distance sensor that shares the same name.
+        sensorDistance = hardwareMap.get(DistanceSensor.class, "revColorSensor");
+
+        dataLogging = new DataLogging("colorSensor", telemetry);
+
+        ElapsedTime timer;
+        double averageLoopCountForMultiReads = 0;
+        double averageLoopCountForMinimumReads = 0;
+
+        // hsvValues is an array that will hold the hue, saturation, and value information.
+        float hsvValues[] = {0F, 0F, 0F};
+
+        // values is a reference to the hsvValues array.
+        final float values[] = hsvValues;
+
+        // sometimes it helps to multiply the raw RGB values with a scale factor
+        // to amplify/attentuate the measured values.
+        final double SCALE_FACTOR = 255;
+
+        // get a reference to the RelativeLayout so we can change the background
+        // color of the Robot Controller app to match the hue detected by the RGB sensor.
+        int relativeLayoutId = hardwareMap.appContext.getResources().getIdentifier("RelativeLayout", "id", hardwareMap.appContext.getPackageName());
+        final View relativeLayout = ((Activity) hardwareMap.appContext).findViewById(relativeLayoutId);
+
+        timer = new ElapsedTime();
+        // wait for the start button to be pressed.
+        waitForStart();
+        dataLogging.startTimer();
+
+        // loop and read the RGB and distance data.
+        // this loop is very inefficient since it constantly reads the color sensor
+        timer.reset();
+        while (opModeIsActive() && loopcount < 1000) {
+            // convert the RGB values to HSV values.
+            // multiply by the SCALE_FACTOR.
+            // then cast it back to int (SCALE_FACTOR is a double)
+            Color.RGBToHSV((int) (sensorColor.red() * SCALE_FACTOR),
+                    (int) (sensorColor.green() * SCALE_FACTOR),
+                    (int) (sensorColor.blue() * SCALE_FACTOR),
+                    hsvValues);
+
+            // send the info back to driver station using telemetry function.
+            telemetry.addData("Distance (cm)",
+                    String.format(Locale.US, "%.02f", sensorDistance.getDistance(DistanceUnit.CM)));
+            telemetry.addData("Alpha", sensorColor.alpha());
+            telemetry.addData("Red  ", sensorColor.red());
+            telemetry.addData("Green", sensorColor.green());
+            telemetry.addData("Blue ", sensorColor.blue());
+            telemetry.addData("Hue         ", hsvValues[0]);
+
+            ////////////dataLogging.logData(String.format(Locale.US, "%.02f", sensorDistance.getDistance(DistanceUnit.CM)) + ", " + hsvValues[0]);
+            // change the background color to match the color detected by the RGB sensor.
+            // pass a reference to the hue, saturation, and value array as an argument
+            // to the HSVToColor method.
+            ////////////relativeLayout.post(new Runnable() {
+            ////////////    public void run() {
+            ////////////        relativeLayout.setBackgroundColor(Color.HSVToColor(0xff, values));
+            ////////////    }
+            ////////////});
+            telemetry.update();
+            loopcount = loopcount + 1;
+            idle();
+        }
+        averageLoopCountForMultiReads = timer.milliseconds() / 1000;
+
+        loopcount = 0;
+        dataLogging.startTimer();
+        timer.reset();
+        // this loop is more efficient. The color sensor is only read once and the values are stored
+        // in variables red, green, blue and distance for use later on. This is instead of reading
+        // the color sensor again later on.
+        while (opModeIsActive() && loopcount < 1000) {
+            red = sensorColor.red();
+            green = sensorColor.green();
+            blue = sensorColor.blue();
+            distance = sensorDistance.getDistance(DistanceUnit.CM);
+
+            // convert the RGB values to HSV values.
+            // multiply by the SCALE_FACTOR.
+            // then cast it back to int (SCALE_FACTOR is a double)
+            Color.RGBToHSV((int) (red * SCALE_FACTOR),
+                    (int) (green * SCALE_FACTOR),
+                    (int) (blue * SCALE_FACTOR),
+                    hsvValues);
+
+            // send the info back to driver station using telemetry function.
+            telemetry.addData("Distance (cm)", String.format(Locale.US, "%.02f", distance));
+            //telemetry.addData("Alpha", sensorColor.alpha());
+            telemetry.addData("Red  ", red);
+            telemetry.addData("Green", green);
+            telemetry.addData("Blue ", blue);
+            telemetry.addData("Hue         ", hsvValues[0]);
+
+            dataLogging.logData(String.format(Locale.US, "%.02f", distance) + ", " + hsvValues[0]);
+            // change the background color to match the color detected by the RGB sensor.
+            // pass a reference to the hue, saturation, and value array as an argument
+            // to the HSVToColor method.
+            ////////////relativeLayout.post(new Runnable() {
+            ////////////    public void run() {
+            ////////////        relativeLayout.setBackgroundColor(Color.HSVToColor(0xff, values));
+            ////////////    }
+            ////////////});
+            telemetry.update();
+            loopcount = loopcount + 1;
+            idle();
+        }
+        averageLoopCountForMinimumReads = timer.milliseconds() / 1000;
+
+        telemetry.addData("Average loop time with inefficient reads: ", String.format("%5.3f", averageLoopCountForMultiReads));
+        telemetry.addData("Average loop time with more efficient reads: ", String.format("%5.3f", averageLoopCountForMinimumReads));
+        telemetry.update();
+
+        dataLogging.logData("Average loop time with inefficient reads: " + String.format("%5.3f", averageLoopCountForMultiReads));
+        dataLogging.logData("Average loop time with more efficient reads: " + String.format("%5.3f", averageLoopCountForMinimumReads));
+
+        // give the user time to read the driver station phone
+        sleep(10000);
+
+        // Set the panel back to the default color
+        relativeLayout.post(new Runnable() {
+            public void run() {
+                relativeLayout.setBackgroundColor(Color.WHITE);
+            }
+        });
+        dataLogging.closeDataLog();
+    }
+}
