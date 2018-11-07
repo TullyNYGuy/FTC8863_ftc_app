0dad2fbb6875cba210df609df471b0c228ab3e9b
TullyNYGuy <glenn_ball@utilitek.com>
2018-11-06 20:11:13 -0500

    Added UDP Receiver to PID Turn

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/GenericTest/TestPidTurn.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/GenericTest/TestPidTurn.java
@@ -6,6 +6,10 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import org.firstinspires.ftc.teamcode.Lib.FTCLib.AdafruitIMU8863;
 import org.firstinspires.ftc.teamcode.Lib.FTCLib.DataLogging;
 import org.firstinspires.ftc.teamcode.Lib.FTCLib.DriveTrain;
+import org.firstinspires.ftc.teamcode.Lib.FTCLib.PidUdpReceiver;
+
+import java.math.RoundingMode;
+import java.text.DecimalFormat;
 
 /**
  * This Opmode is a shell for a linear OpMode. Copy this file and fill in your code as indicated.
@@ -18,7 +22,9 @@ public class TestPidTurn extends LinearOpMode {
 
     // Put your variable declarations here
     DriveTrain driveTrain;
+    PidUdpReceiver pidUdpReceiver;
     double correction;
+    private double p, i, d;
 
 
     @Override
@@ -27,11 +33,29 @@ public class TestPidTurn extends LinearOpMode {
 
         // Put your initializations here
         driveTrain = DriveTrain.DriveTrainAutonomous(hardwareMap, telemetry);
+        pidUdpReceiver = new PidUdpReceiver();
+
+        //
+        telemetry.setMsTransmissionInterval(50);
+        telemetry.addLine("receiver created");
+        telemetry.addLine("receiver listening");
+        telemetry.addLine("transmission interval set");
+        //telemetry.update();
 
         // Wait for the start button
         telemetry.addData(">", "Press Start to run" );
         telemetry.update();
         waitForStart();
+
+        updateCoefficients();
+
+        telemetry.addData("Kp", formatVal(p));
+        telemetry.addData("Ki", formatVal(i));
+        telemetry.addData("Kd", formatVal(d));
+        telemetry.update();
+
+        idle();
+
 
         anyTurn(-90, 0.5);
         telemetry.addData("Finished Turn", "2");
@@ -68,9 +92,30 @@ public class TestPidTurn extends LinearOpMode {
         sleep(3000);
     }
 
+    private void updateCoefficients()
+    {
+        p = pidUdpReceiver.getP();
+        i = pidUdpReceiver.getI();
+        d = pidUdpReceiver.getD();
+    }
+
+    /*
+     * This method formats a raw double for nice display on the DS telemetry
+     */
+    private String formatVal(double val)
+    {
+        DecimalFormat df = new DecimalFormat("#.###");
+        df.setRoundingMode(RoundingMode.CEILING);
+        return df.format(val);
+    }
+
     public void anyTurn(double angle, double power) {
         driveTrain.setupTurn(angle,power, AdafruitIMU8863.AngleMode.RELATIVE);
         driveTrain.pidControl.setThreshold(2.0);
+
+        driveTrain.pidControl.setKp(p);
+        driveTrain.pidControl.setKi(i);
+        driveTrain.pidControl.setKd(d);
 
         while(opModeIsActive() && !driveTrain.updateTurn()) {
             telemetry.addData(">", "Press Stop to end test." );
