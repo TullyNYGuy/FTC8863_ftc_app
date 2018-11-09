5464088dd88e2a45c9b17a82974254542b1d8124
TullyNYGuy <glenn_ball@utilitek.com>
2018-11-08 20:30:08 -0500

    debugging

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/GenericTest/TestPidTurn.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/GenericTest/TestPidTurn.java
@@ -120,9 +120,10 @@ public class TestPidTurn extends LinearOpMode {
         while(opModeIsActive() && !driveTrain.updateTurn()) {
             telemetry.addData(">", "Press Stop to end test." );
             telemetry.addData("Angle = ", "%3.1f", driveTrain.imu.getHeading());
-            telemetry.addData("Kp", formatVal(p));
-            telemetry.addData("Ki", formatVal(i));
-            telemetry.addData("Kd", formatVal(d));
+            telemetry.addData("in anyturn loop", "!");
+            //telemetry.addData("Kp", formatVal(p));
+            //telemetry.addData("Ki", formatVal(i));
+            //telemetry.addData("Kd", formatVal(d));
             telemetry.update();
             idle();
         }
