8329654287e1ee489a18824135441434438e48a7
TullyNYGuy <glenn_ball@utilitek.com>
2018-11-26 20:25:44 -0500

    Started to code for the motor and the lift

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Lib/RoverRuckus/DeliveryLiftSystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Lib/RoverRuckus/DeliveryLiftSystem.java
@@ -5,6 +5,7 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.teamcode.Lib.FTCLib.DcMotor8863;
 import org.firstinspires.ftc.teamcode.Lib.FTCLib.Servo8863;
 
 public class DeliveryLiftSystem {
@@ -23,10 +24,12 @@ public class DeliveryLiftSystem {
     // can be accessed only by this class, or by using the public
     // getter and setter methods
     //*********************************************************************************************
+    private DcMotor8863 liftMotor;
+
     private Servo8863 dumpServo;
-    private double dumpServoHomePosition = 0.1;
-    private double dumpServoDumpPosition = 0.8;
-    private double dumpServoInitPosition = 0.1;
+    private double dumpServoHomePosition = 0.8;
+    private double dumpServoDumpPosition = 0.1;
+    private double dumpServoInitPosition = 0.8;
     private double dumpServoTransferPosition = 0.4;
     //*********************************************************************************************
     //          GETTER and SETTER Methods
@@ -45,6 +48,10 @@ public class DeliveryLiftSystem {
     public DeliveryLiftSystem(HardwareMap hardwareMap, Telemetry telemetry) {
         dumpServo = new Servo8863("dumpServo", hardwareMap, telemetry, dumpServoHomePosition, dumpServoDumpPosition, dumpServoInitPosition, dumpServoInitPosition, Servo.Direction.FORWARD);
         dumpServo.setPositionOne(dumpServoTransferPosition);
+        liftMotor = new DcMotor8863("liftMotor",hardwareMap,telemetry);
+        liftMotor.setMotorType(DcMotor8863.MotorType.ANDYMARK_3_7_ORBITAL);
+        //gear ratio big gear: 76 teeth small is 48
+        liftMotor.setMovementPerRev(8*48/76*1/25.4);
     }
 
     //*********************************************************************************************
@@ -68,4 +75,7 @@ public class DeliveryLiftSystem {
     public void deliveryBoxToTransfer(){
         dumpServo.goPositionOne();
     }
+    public void init(){dumpServo.goHome();}
+    public void update(){}
+    public void shutdown(){dumpServo.goHome();}
 }
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/RoverRuckusTest/DeliveryArmTest.java
@@ -0,0 +1,56 @@
+package org.firstinspires.ftc.teamcode.opmodes.RoverRuckusTest;
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.Lib.RoverRuckus.DeliveryLiftSystem;
+
+/**
+ * This Opmode is a shell for a linear OpMode. Copy this file and fill in your code as indicated.
+ */
+@TeleOp(name = "Delivery Arm Test", group = "Test")
+//@Disabled
+public class DeliveryArmTest extends LinearOpMode {
+
+    // Put your variable declarations here
+    public DeliveryLiftSystem deliveryLiftSystem;
+
+    @Override
+    public void runOpMode() {
+
+
+        // Put your initializations here
+        deliveryLiftSystem = new DeliveryLiftSystem(hardwareMap, telemetry);
+        deliveryLiftSystem.init();
+        // Wait for the start button
+        telemetry.addData(">", "Press Start to run");
+        telemetry.update();
+        waitForStart();
+
+        // Put your calls here - they will not run in a loop
+        deliveryLiftSystem.deliveryBoxToDump();
+        sleep(1000);
+        deliveryLiftSystem.deliveryBoxToTransfer();
+        sleep(1000);
+        deliveryLiftSystem.deliveryBoxToHome();
+        while (opModeIsActive()) {
+
+            // Put your calls that need to run in a loop here
+
+            // Display the current value
+            //telemetry.addData("Motor Speed = ", "%5.2f", powerToRunAt);
+            //telemetry.addData("Encoder Count=", "%5d", motor.getCurrentPosition());
+            telemetry.addData(">", "Press Stop to end test.");
+
+            telemetry.update();
+
+            idle();
+        }
+
+        // Put your cleanup code here - it runs as the application shuts down
+        telemetry.addData(">", "Done");
+        telemetry.update();
+
+    }
+}
