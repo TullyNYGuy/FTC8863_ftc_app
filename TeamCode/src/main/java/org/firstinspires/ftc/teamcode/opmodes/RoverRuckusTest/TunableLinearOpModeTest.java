ed3c5611bc937a06f2dfc4f0e7c8c46b8d1e2dbe
TullyNYGuy <glenn_ball@utilitek.com>
2018-12-25 13:01:11 -0500

    reformatted code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/RoverRuckusTest/TunableLinearOpModeTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/RoverRuckusTest/TunableLinearOpModeTest.java
@@ -15,8 +15,6 @@ import org.firstinspires.ftc.teamcode.Lib.FTCLib.DriveTrain;
 
 /**
  * This Opmode is a shell for a linear OpMode. Copy this file and fill in your code as indicated.
- *
- *
  */
 @TeleOp(name = "Tunable Linear Op Mode", group = "Test")
 //@Disabled
@@ -27,6 +25,7 @@ public class TunableLinearOpModeTest extends TunableLinearOpMode {
     DataLogging dataLogging;
     double kp;
     double ki;
+
     @Override
     public void runOpMode() {
 
@@ -36,30 +35,26 @@ public class TunableLinearOpModeTest extends TunableLinearOpMode {
         dataLogging = new DataLogging("PID", telemetry);
         driveTrain.setDataLogging(dataLogging);
         // Wait for the start button
-        telemetry.addData(">", "Press Start to run" );
+        telemetry.addData(">", "Press Start to run");
         telemetry.update();
         kp = getDouble("kp");
         ki = getDouble("ki");
-        driveTrain.setupTurn(90,04, AdafruitIMU8863.AngleMode.RELATIVE,kp,ki);
+        driveTrain.setupTurn(90, 04, AdafruitIMU8863.AngleMode.RELATIVE, kp, ki);
         waitForStart();
         dataLogging.startTimer();
-        dataLogging.logData("kp ="+kp);
-        dataLogging.logData("ki ="+ki);
+        dataLogging.logData("kp = " + kp);
+        dataLogging.logData("ki = " + ki);
         // Put your calls here - they will not run in a loop
-        while(opModeIsActive()) {
+        while (opModeIsActive()) {
             driveTrain.updateTurn();
 
             telemetry.addData("kp = ", getDouble("kp"));
             telemetry.addData("ki = ", getDouble("ki"));
             telemetry.addData("kd = ", getDouble("kd"));
-
-            // Display the current value
-            //telemetry.addData("Motor Speed = ", "%5.2f", powerToRunAt);
-            //telemetry.addData("Encoder Count=", "%5d", motor.getCurrentPosition());
-            telemetry.addData(">", "Press Stop to end test." );
+            telemetry.addData(">", "Press Stop to end test.");
 
             telemetry.update();
-            
+
             idle();
         }
 
