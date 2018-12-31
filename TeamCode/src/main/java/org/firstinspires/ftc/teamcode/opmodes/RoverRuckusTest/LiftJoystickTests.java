38732518c0d94e3ef7af0c39c02c0819ee28664f
TullyNYGuy <glenn_ball@utilitek.com>
2018-12-31 08:00:48 -0500

    Disabled joystick state machine test.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/RoverRuckusTest/LiftJoystickTests.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/RoverRuckusTest/LiftJoystickTests.java
@@ -1,5 +1,6 @@
 package org.firstinspires.ftc.teamcode.opmodes.RoverRuckusTest;
 
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.util.ElapsedTime;
@@ -10,7 +11,7 @@ import org.firstinspires.ftc.teamcode.Lib.RoverRuckusLib.DeliveryLiftSystem;
  * This Opmode is a shell for a linear OpMode. Copy this file and fill in your code as indicated.
  */
 @TeleOp(name = "Lift State Machine Joystick Tests", group = "Test")
-//@Disabled
+@Disabled
 public class LiftJoystickTests extends LinearOpMode {
 
     // Put your variable declarations here
