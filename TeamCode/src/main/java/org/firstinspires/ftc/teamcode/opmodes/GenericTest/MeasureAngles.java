3a41088c4a2cbdf1bfd98af4435e55895bb11ba4
TullyNYGuy <glenn_ball@utilitek.com>
2019-01-16 21:17:06 -0500

    Changed to measure angles relative

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/GenericTest/MeasureAngles.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/GenericTest/MeasureAngles.java
@@ -67,7 +67,7 @@ public class MeasureAngles extends LinearOpMode {
         telemetry.addData(">", "Press Start to run");
         telemetry.update();
         waitForStart();
-        imu.setAngleMode(AdafruitIMU8863.AngleMode.ABSOLUTE);
+        imu.setAngleMode(AdafruitIMU8863.AngleMode.RELATIVE);
 
 
         // Start the logging of measured acceleration
