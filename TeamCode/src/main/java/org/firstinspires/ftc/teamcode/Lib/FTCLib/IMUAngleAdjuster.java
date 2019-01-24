c938c2e775ce40ec150bade2dce9f04d9c7a6749
TullyNYGuy <glenn_ball@utilitek.com>
2019-01-24 12:51:38 -0500

    Added IMUAngleAdjuster

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Lib/FTCLib/IMUAngleAdjuster.java
@@ -0,0 +1,56 @@
+package org.firstinspires.ftc.teamcode.Lib.FTCLib;
+
+
+public class IMUAngleAdjuster {
+
+    //*********************************************************************************************
+    //          ENUMERATED TYPES
+    //
+    // user defined types
+    //
+    //*********************************************************************************************
+
+
+    //*********************************************************************************************
+    //          PRIVATE DATA FIELDS
+    //
+    // can be accessed only by this class, or by using the public
+    // getter and setter methods
+    //*********************************************************************************************
+
+
+    //*********************************************************************************************
+    //          GETTER and SETTER Methods
+    //
+    // allow access to private data fields for example setMotorPower,
+    // getPositionInTermsOfAttachment
+    //*********************************************************************************************
+
+
+    //*********************************************************************************************
+    //          Constructors
+    //
+    // the function that builds the class when an object is created
+    // from it
+    //*********************************************************************************************
+
+
+    //*********************************************************************************************
+    //          Helper Methods
+    //
+    // methods that aid or support the major functions in the class
+    //*********************************************************************************************
+
+
+    //*********************************************************************************************
+    //          MAJOR METHODS
+    //
+    // public methods that give the class its functionality
+    //*********************************************************************************************
+
+    public double getAdjustedAngle(double angle) {
+        // check the mode (0 to +360, 0 to 360, -180 to 180)
+        // check when the adjustement should triggered
+        return 1.0;
+    }
+}
