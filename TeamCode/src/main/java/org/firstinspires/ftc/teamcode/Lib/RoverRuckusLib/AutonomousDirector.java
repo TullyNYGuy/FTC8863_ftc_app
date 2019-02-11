b89838e5b56b912cefcbc0f76bc597af5b0e637d
TullyNYGuy <glenn_ball@utilitek.com>
2019-02-10 20:29:59 -0500

    started on the logic statements

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Lib/RoverRuckusLib/AutonomousDirector.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Lib/RoverRuckusLib/AutonomousDirector.java
@@ -17,7 +17,19 @@ public class AutonomousDirector {
         DEHANG,
         GO_TO_OUR_CRATER_FROM_CRATERSIDE_LANDER,
         STOP,
-        DELAY
+        DELAY,
+        HIT_GOLD_MINERAL_FROM_FLOOR,
+        HIT_GOLD_MINERAL_FROM_LANDER,
+        CLAIM_DEPOT_FROM_CRATORSIDE_MINERALS,
+        CLAIM_DEPOT_FROM_DEPOTSIDE_MINERALS,
+        CLAIM_DEPOT_FROM_CRATORSIDE_LANDER,
+        CLAIM_DEPOT_FROM_DEPOTSIDE_LANDER,
+        PARK_IN_OUR_CRATER_FROM_CRATERSIDE_LANDER,
+        PARK_IN_OUR_CRATER_FROM_DEPOTSIDE_LANDER,
+        PARK_IN_OUR_CRATER_FROM_CRATER_SIDE_MINERALS,
+        PARK_IN_OTHER_CRATER_FROM_CRATER_SIDE_MINERALS,
+        PARK_IN_OUR_CRATER_FROM_DEPOT_SIDE_MINERALS,
+        PARK_IN_OTHER_CRATER_FROM_DEPOT_SIDE_MINERALS
     }
 
     //*********************************************************************************************
@@ -44,7 +56,7 @@ public class AutonomousDirector {
     // the function that builds the class when an object is created
     // from it
     //*********************************************************************************************
-    public AutonomousDirector(AutonomousConfigurationFile conFigFile){
+    public AutonomousDirector(AutonomousConfigurationFile conFigFile) {
         this.conFigFile = conFigFile;
         taskList = new ArrayList<AutonomousTasks>();
         iterator = taskList.iterator();
@@ -62,25 +74,75 @@ public class AutonomousDirector {
     //
     // public methods that give the class its functionality
     //*********************************************************************************************
-    public AutonomousTasks getNextTask(){
-        if(iterator.hasNext()){
+    public AutonomousTasks getNextTask() {
+        if (iterator.hasNext()) {
             return iterator.next();
-        } else{
+        } else {
             return AutonomousTasks.STOP;
         }
     }
-    private void translator(){
-        if (conFigFile.getSample()== AutonomousConfigurationFile.Sample.CRATER_SIDE || conFigFile.getSample() == AutonomousConfigurationFile.Sample.DEPOT_SIDE || conFigFile.getSample() == AutonomousConfigurationFile.Sample.BOTH){
-           taskList.add(AutonomousTasks.LOCATE_GOLD_MINERAL);
+
+    private boolean isSampling() {
+        return conFigFile.getSample() == AutonomousConfigurationFile.Sample.CRATER_SIDE || conFigFile.getSample() == AutonomousConfigurationFile.Sample.DEPOT_SIDE || conFigFile.getSample() == AutonomousConfigurationFile.Sample.BOTH;
+    }
+
+    private void translator() {
+        if (conFigFile.getSample() == AutonomousConfigurationFile.Sample.CRATER_SIDE || conFigFile.getSample() == AutonomousConfigurationFile.Sample.DEPOT_SIDE || conFigFile.getSample() == AutonomousConfigurationFile.Sample.BOTH) {
+            taskList.add(AutonomousTasks.LOCATE_GOLD_MINERAL);
         }
-        if (conFigFile.getHangLocation() == AutonomousConfigurationFile.HangLocation.CRATER_SIDE || conFigFile.getHangLocation() == AutonomousConfigurationFile.HangLocation.DEPOT_SIDE){
+        if (conFigFile.getHangLocation() == AutonomousConfigurationFile.HangLocation.CRATER_SIDE || conFigFile.getHangLocation() == AutonomousConfigurationFile.HangLocation.DEPOT_SIDE) {
             taskList.add(AutonomousTasks.DEHANG);
         }
-        if (conFigFile.getDelay() != 0){
+        if (conFigFile.getDelay() != 0) {
             taskList.add(AutonomousTasks.DELAY);
             delay = conFigFile.getDelay();
         }
-    }
 
+        if (isSampling()) {
+            taskList.add(AutonomousTasks.HIT_GOLD_MINERAL_FROM_LANDER);
+            if (conFigFile.isClaimDepot()) {
+                if (conFigFile.getHangLocation() == AutonomousConfigurationFile.HangLocation.DEPOT_SIDE) {
+                    taskList.add(AutonomousTasks.CLAIM_DEPOT_FROM_DEPOTSIDE_MINERALS);
+                }
+                if (conFigFile.getHangLocation() == AutonomousConfigurationFile.HangLocation.CRATER_SIDE) {
+                    taskList.add(AutonomousTasks.CLAIM_DEPOT_FROM_CRATORSIDE_MINERALS);
+                }
+            }
+            if (!conFigFile.isClaimDepot()) {
+                if (conFigFile.getHangLocation() == AutonomousConfigurationFile.HangLocation.CRATER_SIDE) {
+                    if (conFigFile.getParkLocation() == AutonomousConfigurationFile.ParkLocation.OUR_CRATER) {
+                        taskList.add(AutonomousTasks.PARK_IN_OUR_CRATER_FROM_CRATER_SIDE_MINERALS);
+                    }
+                    if (conFigFile.getParkLocation() == AutonomousConfigurationFile.ParkLocation.OTHER_CRATER) {
+                        taskList.add(AutonomousTasks.PARK_IN_OTHER_CRATER_FROM_CRATER_SIDE_MINERALS);
+                    }
+                }
+                if (conFigFile.getHangLocation() == AutonomousConfigurationFile.HangLocation.DEPOT_SIDE) {
+                    if (conFigFile.getParkLocation() == AutonomousConfigurationFile.ParkLocation.OUR_CRATER) {
+                        taskList.add(AutonomousTasks.PARK_IN_OUR_CRATER_FROM_DEPOT_SIDE_MINERALS);
+                    }
+                    if (conFigFile.getParkLocation() == AutonomousConfigurationFile.ParkLocation.OTHER_CRATER) {
+                        taskList.add(AutonomousTasks.PARK_IN_OTHER_CRATER_FROM_DEPOT_SIDE_MINERALS);
+                    }
+                }
+            }
+        }
+
+        if (!isSampling() && conFigFile.getParkLocation() == AutonomousConfigurationFile.ParkLocation.OUR_CRATER && !conFigFile.isClaimDepot()) {
+            if (conFigFile.getHangLocation() == AutonomousConfigurationFile.HangLocation.CRATER_SIDE) {
+                taskList.add(AutonomousTasks.PARK_IN_OUR_CRATER_FROM_CRATERSIDE_LANDER);
+            } else {
+                taskList.add(AutonomousTasks.PARK_IN_OUR_CRATER_FROM_DEPOTSIDE_LANDER);
+            }
+        }
+
+        if (!isSampling() && conFigFile.isClaimDepot()) {
+            if (conFigFile.getHangLocation() == AutonomousConfigurationFile.HangLocation.CRATER_SIDE) {
+                taskList.add(AutonomousTasks.CLAIM_DEPOT_FROM_CRATORSIDE_LANDER);
+            } else {
+                taskList.add(AutonomousTasks.CLAIM_DEPOT_FROM_DEPOTSIDE_LANDER);
+            }
+        }
+    }
 }
 
