c3ec5de880b1a52621ab8de707aaba5b2dd87515
TullyNYGuy <glenn_ball@utilitek.com>
2019-02-18 22:31:17 -0500

    Decreased extension of collector arm by 1" because delivery box does not sit over battery anymore.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Lib/RoverRuckusLib/CollectorArm.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Lib/RoverRuckusLib/CollectorArm.java
@@ -384,7 +384,7 @@ public class CollectorArm {
 
     public void goToExtensionArmTransfer() {
         log("COMMANDED EXTENSION ARM TO TRANSFER POSITION");
-        moveToExtensionArmPosition(6.75, 1);
+        moveToExtensionArmPosition(5.75, 1);
     }
 
     public void goToExtensionArm10Inches() {
