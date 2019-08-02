af52423f4cdc6d10ba127b032784f06ed53da7f7
TullyNYGuy <glenn_ball@utilitek.com>
2019-08-01 20:33:39 -0400

    Started working on joystick controlling a motor.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/GenericTest/TanyaTest.java
@@ -0,0 +1,63 @@
+package org.firstinspires.ftc.teamcode.opmodes.GenericTest;
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.Lib.FTCLib.JoyStick;
+
+/**
+ * This Opmode is a shell for a linear OpMode. Copy this file and fill in your code as indicated.
+ */
+@TeleOp(name = "TanyaTest", group = "Test")
+//@Disabled
+public class TanyaTest extends LinearOpMode {
+    final static double JOYSTICK_DEADBAND_VALUE = .15;
+    // Put your variable declarations here
+    JoyStick gamepad1LeftJoyStickX;
+    JoyStick gamepad1LeftJoyStickY;
+    double gamepad1LeftJoyStickXValue = 0;
+    double gamepad1LeftJoyStickYValue = 0;
+
+    JoyStick gamepad1RightJoyStickX;
+    JoyStick gamepad1RightJoyStickY;
+    double gamepad1RightJoyStickXValue = 0;
+    double gamepad1RightJoyStickYValue = 0;
+
+    @Override
+    public void runOpMode() {
+
+
+        // Put your initializations here
+        gamepad1LeftJoyStickX = new JoyStick(JoyStick.JoyStickMode.SQUARE, JOYSTICK_DEADBAND_VALUE, JoyStick.InvertSign.NO_INVERT_SIGN);
+        gamepad1LeftJoyStickY = new JoyStick(JoyStick.JoyStickMode.SQUARE, JOYSTICK_DEADBAND_VALUE, JoyStick.InvertSign.INVERT_SIGN);
+
+        gamepad1RightJoyStickX = new JoyStick(JoyStick.JoyStickMode.SQUARE, JOYSTICK_DEADBAND_VALUE, JoyStick.InvertSign.NO_INVERT_SIGN);
+        gamepad1RightJoyStickY = new JoyStick(JoyStick.JoyStickMode.SQUARE, JOYSTICK_DEADBAND_VALUE, JoyStick.InvertSign.INVERT_SIGN);
+        // Wait for the start button
+        telemetry.addData(">", "Press Start to run");
+        telemetry.update();
+        waitForStart();
+
+        // Put your calls here - they will not run in a loop
+
+        while (opModeIsActive()) {
+
+            // Put your calls that need to run in a loop here
+            gamepad1RightJoyStickYValue = gamepad1RightJoyStickY.scaleInput(gamepad1.right_stick_y);
+            // Display the current value
+            //telemetry.addData("Motor Speed = ", "%5.2f", powerToRunAt);
+            //telemetry.addData("Encoder Count=", "%5d", motor.getCurrentPosition());
+            telemetry.addData(">", "Press Stop to end test.");
+            telemetry.addData("right joystick y value: ", gamepad1RightJoyStickYValue);
+            telemetry.update();
+
+            idle();
+        }
+
+        // Put your cleanup code here - it runs as the application shuts down
+        telemetry.addData(">", "Done");
+        telemetry.update();
+
+    }
+}
