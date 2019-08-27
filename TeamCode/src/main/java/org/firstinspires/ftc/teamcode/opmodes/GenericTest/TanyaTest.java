8e0d9c5a3df7420c40de9de5eac881696ebf3139
Neptune\FTC8863 <ftc8863@gmail.com>
2019-08-27 19:14:05 -0400

    Got Halocontrols and MecanumData to be in the opmode

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/GenericTest/TanyaTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/GenericTest/TanyaTest.java
@@ -4,7 +4,9 @@ import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
+import org.firstinspires.ftc.teamcode.Lib.FTCLib.HaloControls;
 import org.firstinspires.ftc.teamcode.Lib.FTCLib.JoyStick;
+import org.firstinspires.ftc.teamcode.Lib.FTCLib.MecanumData;
 
 /**
  * This Opmode is a shell for a linear OpMode. Copy this file and fill in your code as indicated.
@@ -29,11 +31,14 @@ public class TanyaTest extends LinearOpMode {
 
 
         // Put your initializations here
+
         gamepad1LeftJoyStickX = new JoyStick(JoyStick.JoyStickMode.SQUARE, JOYSTICK_DEADBAND_VALUE, JoyStick.InvertSign.NO_INVERT_SIGN);
         gamepad1LeftJoyStickY = new JoyStick(JoyStick.JoyStickMode.SQUARE, JOYSTICK_DEADBAND_VALUE, JoyStick.InvertSign.INVERT_SIGN);
 
         gamepad1RightJoyStickX = new JoyStick(JoyStick.JoyStickMode.SQUARE, JOYSTICK_DEADBAND_VALUE, JoyStick.InvertSign.NO_INVERT_SIGN);
         gamepad1RightJoyStickY = new JoyStick(JoyStick.JoyStickMode.SQUARE, JOYSTICK_DEADBAND_VALUE, JoyStick.InvertSign.INVERT_SIGN);
+        HaloControls controls = new HaloControls(gamepad1RightJoyStickY, gamepad1RightJoyStickX, gamepad1LeftJoyStickX, this)
+        MecanumData data = new MecanumData();
         // Wait for the start button
         telemetry.addData(">", "Press Start to run");
         telemetry.update();
@@ -49,7 +54,10 @@ public class TanyaTest extends LinearOpMode {
             //telemetry.addData("Motor Speed = ", "%5.2f", powerToRunAt);
             //telemetry.addData("Encoder Count=", "%5d", motor.getCurrentPosition());
             telemetry.addData(">", "Press Stop to end test.");
-            telemetry.addData("right joystick y value: ", gamepad1RightJoyStickYValue);
+            controls.getMechanumdata(data);
+            telemetry.addData("translation angle: ", data.getAngleOfTranslation());
+            telemetry.addData("speed: ", data.getSpeed());
+            telemetry.addData("speed of rotation: ", data.getSpeedOfRotation());
             telemetry.update();
 
             idle();
