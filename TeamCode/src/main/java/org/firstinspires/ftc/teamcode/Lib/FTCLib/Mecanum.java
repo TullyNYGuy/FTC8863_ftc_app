ef7702914a4c4e273718c95680e9eccbd00a0f9f
TullyNYGuy <glenn_ball@utilitek.com>
2019-09-16 22:21:15 -0400

    Reformatted code.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Lib/FTCLib/Mecanum.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Lib/FTCLib/Mecanum.java
@@ -47,25 +47,25 @@ public class Mecanum {
             return backRight;
         }
 
-        public Wheels(){
+        public Wheels() {
             frontLeft = 0;
             frontRight = 0;
             backLeft = 0;
             backRight = 0;
         }
 
-        private Wheels scale4Numbers(Wheels wheels){
+        private Wheels scale4Numbers(Wheels wheels) {
             double biggerNumber = Math.abs(wheels.frontLeft);
-            if (biggerNumber < Math.abs(wheels.frontRight)){
+            if (biggerNumber < Math.abs(wheels.frontRight)) {
                 biggerNumber = Math.abs(wheels.frontRight);
             }
-            if (biggerNumber < Math.abs(wheels.backRight)){
+            if (biggerNumber < Math.abs(wheels.backRight)) {
                 biggerNumber = Math.abs(wheels.backRight);
             }
-            if (biggerNumber < Math.abs(wheels.backLeft)){
+            if (biggerNumber < Math.abs(wheels.backLeft)) {
                 biggerNumber = Math.abs(wheels.backLeft);
             }
-            if (biggerNumber != 0 && biggerNumber > 1){
+            if (biggerNumber != 0 && biggerNumber > 1) {
                 wheels.frontRight = wheels.frontRight / biggerNumber;
                 wheels.frontLeft = wheels.frontLeft / biggerNumber;
                 wheels.backRight = wheels.backRight / biggerNumber;
@@ -88,7 +88,7 @@ public class Mecanum {
     // the function that builds the class when an object is created
     // from it
     //*********************************************************************************************
-    public Mecanum(){
+    public Mecanum() {
         wheels = new Wheels();
     }
 
@@ -105,22 +105,23 @@ public class Mecanum {
     // public methods that give the class its functionality
     //*********************************************************************************************
     //if speed of rotation is = 0 then our max speed is 0.707. We may want to scale up to 1.
-    public Wheels calculateWheelVelocity (MecanumData mecanumData){
-        wheels.frontLeft = mecanumData.getSpeed() * Math.sin(-mecanumData.getAngleOfTranslation() + (Math.PI/4)) - mecanumData.getSpeedOfRotation();
-        wheels.frontRight = mecanumData.getSpeed() * Math.cos(-mecanumData.getAngleOfTranslation() + (Math.PI/4)) + mecanumData.getSpeedOfRotation();
-        wheels.backLeft = mecanumData.getSpeed() * Math.cos(-mecanumData.getAngleOfTranslation() + (Math.PI/4)) - mecanumData.getSpeedOfRotation();
-        wheels.backRight = mecanumData.getSpeed() * Math.sin(-mecanumData.getAngleOfTranslation() + (Math.PI/4)) + mecanumData.getSpeedOfRotation();
-    return wheels.scale4Numbers(wheels);
+    public Wheels calculateWheelVelocity(MecanumData mecanumData) {
+        wheels.frontLeft = mecanumData.getSpeed() * Math.sin(-mecanumData.getAngleOfTranslation() + (Math.PI / 4)) - mecanumData.getSpeedOfRotation();
+        wheels.frontRight = mecanumData.getSpeed() * Math.cos(-mecanumData.getAngleOfTranslation() + (Math.PI / 4)) + mecanumData.getSpeedOfRotation();
+        wheels.backLeft = mecanumData.getSpeed() * Math.cos(-mecanumData.getAngleOfTranslation() + (Math.PI / 4)) - mecanumData.getSpeedOfRotation();
+        wheels.backRight = mecanumData.getSpeed() * Math.sin(-mecanumData.getAngleOfTranslation() + (Math.PI / 4)) + mecanumData.getSpeedOfRotation();
+        return wheels.scale4Numbers(wheels);
     }
-    public void test(Telemetry telemetry){
+
+    public void test(Telemetry telemetry) {
         MecanumData mecanumData = new MecanumData();
         mecanumData.setSpeedOfRotation(0);
-        mecanumData.setAngleOfTranslation(-Math.PI/2);
+        mecanumData.setAngleOfTranslation(-Math.PI / 2);
         mecanumData.setSpeed(0.5);
         Wheels wheels = calculateWheelVelocity(mecanumData);
-        telemetry.addData("front left = ",wheels.frontLeft);
-        telemetry.addData("front right = ",wheels.frontRight);
-        telemetry.addData("back left = ",wheels.backLeft);
-        telemetry.addData("back right = ",wheels.backRight);
+        telemetry.addData("front left = ", wheels.frontLeft);
+        telemetry.addData("front right = ", wheels.frontRight);
+        telemetry.addData("back left = ", wheels.backLeft);
+        telemetry.addData("back right = ", wheels.backRight);
     }
 }
